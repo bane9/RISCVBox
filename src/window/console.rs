03494330817975484ffb06e6b91af0512ea40213
Branislav Brzak <bbranislav99@gmail.com>
2024-08-30 00:49:54 +0200

    Properly restore old console settings at exit

--- src/window/console.rs
+++ src/window/console.rs
@@ -7,6 +7,8 @@ pub mod win32_console_settings {
     use winapi::um::processenv::GetStdHandle;
     use winapi::um::wincon::{ENABLE_ECHO_INPUT, ENABLE_LINE_INPUT, ENABLE_PROCESSED_INPUT};
 
+    static mut OLD_CONSOLE_MODE: Option<winapi::shared::minwindef::DWORD> = None;
+
     extern "C" {
         #[link_name = "SetConsoleMode"]
         fn SetConsoleMode(
@@ -18,6 +20,22 @@ pub mod win32_console_settings {
             handle: winapi::um::winnt::HANDLE,
             mode: *mut winapi::shared::minwindef::DWORD,
         ) -> winapi::shared::minwindef::BOOL;
+        #[link_name = "atexit"]
+        fn atexit(callback: unsafe extern "C" fn());
+    }
+
+    unsafe extern "C" fn set_old_console_mode() {
+        if let Some(mode) = OLD_CONSOLE_MODE {
+            let handle = GetStdHandle(winapi::um::winbase::STD_INPUT_HANDLE);
+            if handle == INVALID_HANDLE_VALUE {
+                eprintln!("Failed to get console handle");
+                return;
+            }
+
+            if SetConsoleMode(handle, mode) == 0 {
+                eprintln!("Failed to set console mode");
+            }
+        }
     }
 
     pub fn set_interactive_console() {
@@ -34,14 +52,17 @@ pub mod win32_console_settings {
                 return;
             }
 
-            let new_mode = mode
-                & !(ENABLE_LINE_INPUT
-                    | ENABLE_ECHO_INPUT
-                    | if false { ENABLE_PROCESSED_INPUT } else { 0 });
+            if OLD_CONSOLE_MODE.is_none() {
+                OLD_CONSOLE_MODE = Some(mode);
+            }
+
+            let new_mode = mode & !(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT);
 
             if SetConsoleMode(handle, new_mode) == 0 {
                 eprintln!("Failed to set console mode");
             }
+
+            atexit(set_old_console_mode);
         }
     }
 }
@@ -52,13 +73,33 @@ pub mod posix_console_settings {
 
     use termios::{tcsetattr, Termios, TCSAFLUSH};
 
+    static mut OLD_TERMINAL_MODE: Option<Termios> = None;
+
+    extern "C" fn set_old_terminal_mode() {
+        unsafe {
+            if let Some(termios) = OLD_TERMINAL_MODE {
+                tcsetattr(0, TCSAFLUSH, &termios).unwrap();
+            }
+        }
+    }
+
     pub fn set_interactive_console() {
         let mut termios = Termios::from_fd(0).unwrap();
+
+        if let None = unsafe { OLD_TERMINAL_MODE } {
+            unsafe {
+                OLD_TERMINAL_MODE = Some(termios);
+            }
+        }
 
         termios.c_lflag &= !(termios::ECHO | termios::ICANON | termios::ISIG | termios::IEXTEN);
 
         termios.c_iflag &= !(termios::IXON | termios::ICRNL);
 
         tcsetattr(0, TCSAFLUSH, &termios).unwrap();
+
+        unsafe {
+            libc::atexit(set_old_terminal_mode);
+        }
     }
 }
